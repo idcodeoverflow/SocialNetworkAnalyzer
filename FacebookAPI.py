eae3755c0ddbc9da4392e44420c887f50b97d09b
David Garcia <david.programming@gmail.com>
2015-02-24 12:00:29 -0600

    Changes in API.

--- FacebookAPI.py
+++ FacebookAPI.py
@@ -1,8 +1,9 @@
-from DBLayout.FacebookUserDB import FacebookUserDB
-from EntitiesLayout.FacebookUser import FacebookUser
 import json
 import urllib.request
 import urllib.error
+
+from DBLayout.FacebookUserDB import FacebookUserDB
+
 
 __author__ = 'David'
 
@@ -76,74 +77,103 @@ class FacebookAPI:
         return result
 
     def getPosts(self):
-        self.currentUserId = self._START_NUMBER_
+
+        users = []
+        userAccess = FacebookUserDB()
         result = []
-        control = 0
+        try:
+            users = userAccess.readUsers()
+        except Exception as e:
+            print(e)
 
 
-        while control < self.usersNumber:
+        for i in users:
 
-            self.existsUser = False
+            self.currentUserId = i.facebookUserId
+            self.existsPost = False
 
             try:
-                packetData = urllib.request.urlopen(self.url + str(self.currentUserId), data=None, timeout=500, cafile=None,
+                packetData = urllib.request.urlopen(self.url + str(self.currentUserId) + '/statuses', data=None, timeout=500, cafile=None,
                                                     capath=None,
                                                     cadefault=False)
             except urllib.error.HTTPError as e:
-                self.existsUser = True
+                self.existsPost = True
                 self.limit += 1
                 print(e.fp.read())
 
             jsonString = packetData.read().decode('utf-8')
 
-            if not self.existsUser:
+            if not self.existsPost:
 
                 data = json.loads(jsonString)
 
-                try:
-                    data['link']
-                except KeyError:
-                    print("User ID: " + str(self.currentUserId) + " link is not available, will be discarded.")
-                    self.currentUserId += 1
-                    continue
-
                 if self.printable:
-                    print('--*--*--*--*--*--*--*--*--*--*--*--*-- USER ' + str(
+                    print('--*--*--*--*--*--*--*--*--*--*--*--*-- POST ' + str(
                         self.currentUserId) + ' --*--*--*--*--*--*--*--*--*--*--*--*--')
 
                     for y in data.keys():
                         print('%s: %s' % (y, data[y]))
 
-                control += 1
 
                 result.append(data)
 
             else:
                 if self.printable:
-                    print('User ID: %s not found' % self.currentUserId)
+                    print('Post from User ID: %s not found' % self.currentUserId)
 
             packetData.close()
 
             self.currentUserId += 1
 
-            print(str(control / self.usersNumber) + '% completed. ' + str(control) + ' of ' + str(self.usersNumber))
-
         return result
 
-# fb = FacebookAPI(0, "", True)
-#
-# for i in range(0,450,1):
-#
-#     user = fb.currentUserId
-#     fb = FacebookAPI(20, "", True)
-#     fb.currentUserId = user
-#     users = fb.getUsers()
-#     userAccess = FacebookUserDB()
-#     for data in users:
-#         user = FacebookUser(data)
-#         userAccess.insertUser(user)
-#         print(data)
-userAccess = FacebookUserDB()
-for i in userAccess.readUsers():
-    print(i)
 
+    def getProfilePage(fbUser: FacebookUser, printable: bool):
+        profilePage = ''
+            self.existsPost = False
+
+            try:
+                packetData = urllib.request.urlopen(self.url + str(self.currentUserId) + '/statuses', data=None, timeout=500, cafile=None,
+                                                    capath=None,
+                                                    cadefault=False)
+            except urllib.error.HTTPError as e:
+                self.existsPost = True
+                self.limit += 1
+                print(e.fp.read())
+
+            jsonString = packetData.read().decode('utf-8')
+
+            if not self.existsPost:
+
+                data = json.loads(jsonString)
+
+                if self.printable:
+                    print('--*--*--*--*--*--*--*--*--*--*--*--*-- POST ' + str(
+                        self.currentUserId) + ' --*--*--*--*--*--*--*--*--*--*--*--*--')
+
+                    for y in data.keys():
+                        print('%s: %s' % (y, data[y]))
+
+
+                result.append(data)
+
+            else:
+                if self.printable:
+                    print('Post from User ID: %s not found' % self.currentUserId)
+
+            packetData.close()
+
+            self.currentUserId += 1
+
+        return result
+        return
+
+fb = FacebookAPI(0, "", True)
+
+for i in range(0,450,1):
+
+    user = fb.currentUserId
+    fb = FacebookAPI(20, "", True)
+    fb.currentUserId = user
+    users = fb.getPosts()
+    userAccess = FacebookUserDB()
