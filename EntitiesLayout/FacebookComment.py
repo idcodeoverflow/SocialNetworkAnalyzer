018a2a7098b486939f57db29a64e20ba15bd0ac7
David Garcia <david.programming@gmail.com>
2015-05-04 12:47:36 -0500

    Update Comment Managing classes.

--- EntitiesLayout/FacebookComment.py
+++ EntitiesLayout/FacebookComment.py
@@ -5,23 +5,53 @@ __author__ = 'David'
 
 class FacebookComment:
 
-    facebookCommentID = 0
-    fbId = 0
-    legacyId = 0
-    author = 0
-    time = 0
-    text = ""
-    likesCount = 0
-    facebookPostID = 0
+    def __init__(self):
+        self.facebookCommentId = 0
+        self.id = 0
+        self.fbid = 0
+        self.legacyId = 0
+        self.body = CommentBody('', [], [])
+        self.author = 0
+        self.ftIdentifier = 0
+        self.isFeatured = False
+        self.likeCount = 0
+        self.hasViewerLiked = False
+        self.canRemove = False
+        self.canReport = False
+        self.canEdit = False
+        self.source = ""
+        self.viewerCanLike = False
+        self.canCommnet = False
+        self.isAuthorWeakReference = False
+        self.isTranslatable = False
+        self.timestamp = Timestamp(0, '', '')
+        self.interestingReplyOffset = 0
+        self.interestingReplyId = 0
+        self.recentReplyTimestamp = Timestamp(0, '', '')
+        self.spamReplyCount = Timestamp(0, '', '')
 
-    def __init__(self, mapa, postId: int):
-        try:
-            self.facebookCommentID = mapa['id']
-            self.fbId = mapa['fbid']
-            self.legacyId = mapa['legacyid']
-            self.text = (mapa['body'])['text']
-            self.time = (mapa['timestamp'])['time']
-            self.likesCount = mapa['likecount']
-            self.facebookPostID = postId
-        except KeyError:
-            print('A key is missing, register will be discarded')
+
+
+class CommentBody:
+
+    text = ''
+    ranges = []
+    aggregatedRanges = []
+
+    def __init__(self, text: str, ranges: [], agregatedRanges: []):
+        self.text = text
+        self.ranges = ranges
+        self.aggregatedRanges = agregatedRanges
+
+class Timestamp:
+
+    time = 0
+    text = ''
+    verbose = ''
+
+    def __init__(self, time: int, text: str, verbose: str):
+        self.time = time
+        self.text = text
+        self.verbose = verbose
+
+
